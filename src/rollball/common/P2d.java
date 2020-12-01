5a9cf0ee0c5ae6b5dcdf437ff38c30ff2f343f7f
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-01 19:09:16 +0100

    Added P2d and V2d impl

---
+++ src/rollball/common/P2d.java
@@ -0,0 +1,59 @@
+package rollball.common;
+
+/**
+ * A 2-dimensional point.
+ */
+public class P2d {
+    /**
+     * The point abscissa.
+     */
+    private final double x;
+    /**
+     * The point ordinate.
+     */
+    private final double y;
+
+    /**
+     * @param x
+     *          the point abscissa
+     * @param y
+     *          the point ordinate
+     */
+    public P2d(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * 
+     * @param p
+     * @return a new V2d
+     */
+    public V2d sub(final P2d p) {
+        return new V2d(this.x - p.getX(), this.y - p.getY());
+    }
+
+    /**
+     * 
+     * @param v
+     * @return a new P2d
+     */
+    public P2d sum(final V2d v) {
+        return new P2d(this.x + v.getX(), this.y + v.getY());
+    }
+
+    /**
+     * @return the x
+     */
+    public double getX() {
+        return this.x;
+    }
+
+    /**
+     * @return the y
+     */
+    public double getY() {
+        return this.y;
+    }
+
+}
---
+++ src/rollball/common/V2d.java
@@ -0,0 +1,68 @@
+package rollball.common;
+
+/**
+ * A 2-dimensional vector.
+ */
+public class V2d {
+
+    private final double x;
+    private final double y;
+
+    /**
+     * @param x
+     * @param y
+     */
+    public V2d(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @param v
+     *          the V2d vector to sum
+     * @return a new V2d, result of the vectors sum.
+     */
+    public V2d sum(final V2d v) {
+        return new V2d(this.x + v.x, this.y + v.y);
+    }
+
+    /**
+     * @return the vector modulus.
+     */
+    public double module() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    /**
+     * @return a new V2d, representing the vector normalization.
+     */
+    public V2d getNormalized() {
+        final double module = this.module();
+        return new V2d(this.x / module, this.y / module);
+    }
+
+    /**
+     * 
+     * @param fact
+     *          the multiplication factor
+     * @return a new V2d, result of the vector multiplication.
+     */
+    public V2d mul(final double fact) {
+        return new V2d(this.x * fact, this.y * fact);
+    }
+
+    /**
+     * @return the x
+     */
+    public double getX() {
+        return this.x;
+    }
+
+    /**
+     * @return the y
+     */
+    public double getY() {
+        return this.y;
+    }
+
+}

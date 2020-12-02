f369f75e56b2492266f586f1474cc62c0826a471
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-02 21:30:43 +0100

    Command interface created

---
+++ src/rollball/input/Command.java
@@ -0,0 +1,16 @@
+package rollball.input;
+
+import rollball.model.World;
+
+/**
+ * COMMAND PATTERN. 
+ */
+public interface Command {
+
+    /**
+     * Update the world consistently with 
+     * the received inputs.
+     */
+    public void execute(World scene);
+
+}

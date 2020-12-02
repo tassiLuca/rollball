5bcfa81e86baf126b8fb67c6cd65b71f9a103c43
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-02 20:54:00 +0100

    Adapt size to screen resolution and minor improvements

--- src/rollball/graphics/Scene.java
+++ src/rollball/graphics/Scene.java
@@ -7,8 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Stroke;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.Toolkit;
 import java.util.Set;
 
 import javax.swing.JFrame;
@@ -20,48 +19,44 @@ import rollball.model.GameObject;
 import rollball.model.PickUpObj;
 import rollball.model.World;
 
-public class Scene {
+public final class Scene {
 
-    private final JFrame frame;
+    private final JFrame frame = new JFrame("Roll A Ball");
     private final World world;
 
-    public Scene(final World world, final int w, final int h) {
-        frame = new JFrame("Roll A Ball");
-        frame.setSize(w, h);
-        frame.setMinimumSize(new Dimension(w, h));
-        frame.setResizable(false);
+    public Scene(final World world) {
         this.world = world;
-        final ScenePanel panel = new ScenePanel(w, h);
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth(); 
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw, sh);
+        frame.setMinimumSize(new Dimension(sw, sh));
+        frame.setResizable(false);
+        frame.setLocationByPlatform(true);
+        final ScenePanel panel = new ScenePanel(sw, sh);
         frame.getContentPane().add(panel);
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(final WindowEvent e) {
-                System.exit(-1);
-            }
-            public void windowClosed(final WindowEvent e) {
-                System.exit(-1);
-            }
-        });
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
-    public final void render() {
+    public void render() {
         frame.repaint();
     }
 
-    public class ScenePanel extends JPanel {
+    private final class ScenePanel extends JPanel {
 
         private static final long serialVersionUID = 1L;
         private final int centerX;
         private final int centerY;
 
-        public ScenePanel(final int w, final int h) {
+        private ScenePanel(final int w, final int h) {
             this.setSize(w, h);
             this.centerX = w / 2;
             this.centerY = h / 2;
         }
 
-        public final void paint(final Graphics g) {
+        public void paint(final Graphics g) {
             final Graphics2D g2 = (Graphics2D) g;
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
                     RenderingHints.VALUE_ANTIALIAS_ON);
@@ -78,11 +73,11 @@ public class Scene {
                 if (obj instanceof Ball) {
                         g2.setColor(Color.BLUE);
                         g2.setStroke(strokeBall);
-                        g2.drawOval(x-20, y-20, 40, 40);
+                        g2.drawOval(x - 20, y - 20, 40, 40);
                 } else if (obj instanceof PickUpObj) {
                         g2.setColor(Color.RED);
                         g2.setStroke(strokePick);
-                        g2.drawRect(x-20, y-20, 40, 40);
+                        g2.drawRect(x - 20, y - 20, 40, 40);
                 }
             });
         }

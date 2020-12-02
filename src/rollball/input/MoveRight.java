c7b3116969b379125fa8c83aec872427bc702e24
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-02 21:35:59 +0100

    MoveLeft and Right impl

---
+++ src/rollball/input/MoveRight.java
@@ -0,0 +1,14 @@
+package rollball.input;
+
+import rollball.common.V2d;
+import rollball.model.Ball;
+import rollball.model.World;
+
+public class MoveRight implements Command {
+    @Override
+    public void execute(final World world) {
+        final Ball ball = world.getBall();
+        final double speed = ball.getCurrentVel().module();
+        ball.setVel(new V2d(1, 0).mul(speed));
+    }
+}

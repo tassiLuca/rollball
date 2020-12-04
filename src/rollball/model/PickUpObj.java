1d524d2c9b20bfe2e206dc69d01c43c4fab3bb24
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-05 00:31:45 +0100

    Added docs

--- src/rollball/model/PickUpObj.java
+++ src/rollball/model/PickUpObj.java
@@ -5,13 +5,26 @@ import rollball.common.V2d;
 
 public class PickUpObj extends AbstractGameObject {
 
+    /**
+     * The edge length. 
+     */
     private final double edge;
 
+    /**
+     * Initialize a new pickup object.
+     * @param pos
+     *          the central position
+     * @param edge
+     *          the edge length
+     */
     public PickUpObj(final P2d pos, final double edge) {
         super(pos, new V2d(0, 0), new CircleBoundingBox(new P2d(pos.getX(), pos.getY()), edge / 2));
         this.edge = edge;
     }
 
+    /**
+     * @return the edge length
+     */
     public double getEdge() {
         return this.edge;
     }

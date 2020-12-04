cb7909241c0fa38bde880a024af70c8dcedccae2
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-05 00:29:46 +0100

    Added final and documentations

--- src/rollball/model/CircleBoundingBox.java
+++ src/rollball/model/CircleBoundingBox.java
@@ -3,21 +3,34 @@ package rollball.model;
 import rollball.common.P2d;
 import rollball.common.V2d;
 
+/**
+ * Specialize a bounding box for circle shapes.
+ */
 public class CircleBoundingBox implements BoundingBox {
-
+    /**
+     * The circle center point.
+     */
     private final P2d center;
+    /**
+     * The circle radius.
+     */
     private final double radius;
 
     /**
+     * Initialize a new CircleBoundingBox.
      * @param center
+     *          the circle center point
      * @param radius
+     *          the circle radius
      */
-    public CircleBoundingBox(P2d center, double radius) {
+    public CircleBoundingBox(final P2d center, final double radius) {
         this.center = center;
         this.radius = radius;
     }
 
-    @Override
+    /**
+     * {@inheritDoc}
+     */
     public boolean isCollidingWith(final P2d p, final double radius) {
         return new V2d(p, center).module() <= radius + this.radius;
     }

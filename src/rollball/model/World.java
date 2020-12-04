e2581f31c1f10a6dcc56d456cec39140aaf20bdb
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-04 22:37:40 +0100

    CheckBoundaries impl

--- src/rollball/model/World.java
+++ src/rollball/model/World.java
@@ -3,6 +3,8 @@ package rollball.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import rollball.common.P2d;
+
 public class World {
 
     /**
@@ -13,6 +15,11 @@ public class World {
      * The set of pickup objects.
      */
     private final Set<GameObject> picks = new HashSet<>();
+    private RectBoundingBox worldBox;
+
+    public World(final RectBoundingBox box) {
+        this.worldBox = box;
+    }
 
     /**
      * Set the world ball.
@@ -38,6 +45,31 @@ public class World {
         this.picks.add(obj);
     }
 
+    private void checkBoundaries() {
+        final P2d ballPos = this.ball.getCurrentPos();
+        final double ballRadius = this.ball.getRadius();
+        final double upperLimit = this.worldBox.getUpperLeftCorner().getY();
+        final double bottomLimit = this.worldBox.getBottomRightCorner().getY();
+        final double leftLimit = this.worldBox.getUpperLeftCorner().getX();
+        final double rightLimit = this.worldBox.getBottomRightCorner().getX();
+        //
+        if (ballPos.getY() + ballRadius >= upperLimit) {
+            this.ball.setPos(new P2d(ballPos.getX(), upperLimit - ballRadius));
+            this.ball.flipVelOnY();
+        } else if (ballPos.getY() - ballRadius <= bottomLimit) {
+            this.ball.setPos(new P2d(ballPos.getX(), bottomLimit + ballRadius));
+            this.ball.flipVelOnY();
+        }
+        //
+        if (ballPos.getX() + ballRadius >= rightLimit) {
+            this.ball.setPos(new P2d(rightLimit - ballRadius, ballPos.getY()));
+            this.ball.flipVelOnX();
+        } else if (ballPos.getX() - ballRadius <= leftLimit) {
+            this.ball.setPos(new P2d(rightLimit + ballRadius, ballPos.getY()));
+            this.ball.flipVelOnX();
+        }
+    }
+
     /**
      * Update the state of game objects.
      * @param dt
@@ -45,6 +77,7 @@ public class World {
     public void updateWorld(final int dt) {
         this.picks.stream().forEach(obj -> obj.updateState(dt));
         this.ball.updateState(dt);
+        checkBoundaries();
     }
 
     /**

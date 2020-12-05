f0f9deabb0fa1a9360a555e4d4a3757d4c76aab9
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-05 13:37:36 +0100

    Added documentation

--- src/rollball/model/events/WorldEventListener.java
+++ src/rollball/model/events/WorldEventListener.java
@@ -2,6 +2,12 @@ package rollball.model.events;
 
 public interface WorldEventListener {
 
+    /**
+     * Add the event to the command queue ready
+     * to be executed.
+     * @param ev
+     *          the next command to be executed
+     */
     void notifyEvent(WorldEvent ev);
 
 }

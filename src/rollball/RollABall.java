e6fff5dfb8a90cc0adde5182d01af9b8e0b17204
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2020-12-05 16:13:11 +0100

    Added a sleep at the start of the game

--- src/rollball/RollABall.java
+++ src/rollball/RollABall.java
@@ -6,8 +6,9 @@ public final class RollABall {
 
     private RollABall() { }
 
-    public static void main(final String...args) {
+    public static void main(final String...args) throws InterruptedException {
         final GameEngine engine = new GameEngine();
+        Thread.sleep(1000);
         engine.mainLoop();
     }
 
